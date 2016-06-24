a16a779fd65412137d0caee45f4dae8fcd08a377
Hitoshi Okonogi <hitotito@gmail.com>
2016-06-24 15:18:24 -0700

    Fixes Debian 8.5 base box preseeds

--- debian-8.5/scripts/virtualbox.sh
+++ debian-8.5/scripts/virtualbox.sh
@@ -1,1 +1,10 @@
-apt-get -y install virtualbox-guest-utils
+#apt-get -y install virtualbox-guest-utils
+sudo apt-get -y install wget make gcc
+wget http://download.virtualbox.org/virtualbox/5.0.22/VBoxGuestAdditions_5.0.22.iso
+sudo mkdir /media/VBoxGuestAdditions
+sudo mount -o loop,ro VBoxGuestAdditions_5.0.22.iso /media/VBoxGuestAdditions
+sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
+rm VBoxGuestAdditions_5.0.22.iso
+sudo umount /media/VBoxGuestAdditions
+sudo rmdir /media/VBoxGuestAdditions
+sudo rm VBoxGuestAdditions_5.0.22.iso
