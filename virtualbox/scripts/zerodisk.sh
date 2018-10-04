6fb1632e24cb68710fd34438655c853c51e02c61
Tosh Okonogi <hitotito@gmail.com>
2018-10-04 16:08:15 -0700

    Update Debian9 (Stretch) to 9.5

--- virtualbox/scripts/zerodisk.sh
+++ virtualbox/scripts/zerodisk.sh
@@ -1,3 +1,3 @@
-dd if=/dev/zero of=/EMPTY bs=1M || echo "....finished"
-sudo rm -f /EMPTY && echo "Zero disk successful"
+#dd if=/dev/zero of=/EMPTY bs=1M || echo "....finished"
+#sudo rm -f /EMPTY && echo "Zero disk successful"
 exit 0
