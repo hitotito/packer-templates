b0818c812541a94d9a6dac7db97ec22265f2eb45
Tosh Okonogi <hitotito@gmail.com>
2018-12-21 14:45:09 -0800

    Add raid configuration option

--- virtualbox/packer_helper.py
+++ virtualbox/packer_helper.py
@@ -11,15 +11,14 @@ PACKER_VARIABLES = {
     "packer_http_port": "{{ .HTTPPort }}",
 }
 
-TEMPLATE_FILE = "template.json.j2"
-CONFIG_FILE = "config.yml"
+DEFAULT_CONFIG_FILE = "config.yml"
 
 ERROR_SUCCESS = 0
 ERROR_NO_TARGET = 1
 ERROR_INVALID_TARGET = 2
 
-def get_config():
-    return yaml.load(open(CONFIG_FILE, 'r').read())
+def get_config(config_file_path):
+    return yaml.load(open(config_file_path, 'r').read())
 
 def get_buildable_targets(config):
     return [key for key in config.keys() if key not in UNBUILDABLE_TARGET]
@@ -36,17 +35,19 @@ def entry_point():
     pass
 
 @entry_point.command()
-def list_targets():
-    config = get_config()
+@click.option('-c', '--config', 'config_path', default=DEFAULT_CONFIG_FILE, help="configuration file path")
+def list_targets(config_path):
+    config = get_config(config)
     print (" ".join(sorted(get_buildable_targets(config))))
 
 @entry_point.command()
 @click.argument('target', required=True, nargs=1)
-def template(target):
+@click.option('-c', '--config', 'config_path', default=DEFAULT_CONFIG_FILE, help="configuration file path")
+def template(target, config_path):
     """
     """
     # Check if specified target is in configuration
-    config = yaml.load(open(CONFIG_FILE, 'r').read())
+    config = get_config(config_path)
     available_targets = get_buildable_targets(config)
     if not target in available_targets:
         print ("Error: target [{}] is not available. Please specify a valid target.".format(target), file=sys.stderr)
@@ -54,7 +55,7 @@ def template(target):
         sys.exit(ERROR_INVALID_TARGET)
 
     variables = get_variables(config, target)
-    build_template = Template(open(TEMPLATE_FILE, 'r').read())
+    build_template = Template(open(variables.get('template'), 'r').read())
     print (build_template.render({**variables, **PACKER_VARIABLES}))
     sys.exit(ERROR_SUCCESS)
 
