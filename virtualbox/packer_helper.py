14585e1e585e3392311d8aa2c5582932d252933f
Tosh Okonogi <hitotito@gmail.com>
2018-12-21 23:16:14 -0800

    Add inheritance in configuration

--- virtualbox/packer_helper.py
+++ virtualbox/packer_helper.py
@@ -1,4 +1,5 @@
 import click
+import json
 import sys
 import yaml
 from jinja2 import Template
@@ -18,6 +19,14 @@ ERROR_SUCCESS = 0
 ERROR_NO_TARGET = 1
 ERROR_INVALID_TARGET = 2
 
+def check_target(config, target):
+    available_targets = get_buildable_targets(config)
+    if not target in available_targets:
+        # TODO : convert to exception
+        print ("Error: target [{}] is not available. Please specify a valid target.".format(target), file=sys.stderr)
+        print ("  Available targets:\n\t{}".format("\n\t".join(sorted(available_targets))), file=sys.stderr)
+        sys.exit(ERROR_NO_TARGET) 
+
 def get_config(config_file_path):
     return yaml.load(open(config_file_path, 'r').read())
 
@@ -26,9 +35,17 @@ def get_buildable_targets(config):
 
 def get_variables(config, target):
     """
-    Get global variables and we will extend target variables on top of it
+    Method that will compute target variables from config.
+    A few important mentions:
+    `global` key will always be applied
+    `_inherit` key can be used to create inheritance. If same key exists,
+    higher level variables will override variables.
     """
-    return dict(config.get('global',{}), **config.get(target, {}))
+    target_config = config.get(target, {})
+    base_config = target_config.pop('_inherit', None)
+    if base_config:
+        return dict(get_variables(config, base_config), **target_config)
+    return dict(config.get('global',{}), **target_config)
 
 # cli interfaces
 @click.group()
@@ -49,15 +66,25 @@ def template(target, config_path):
     """
     # Check if specified target is in configuration
     config = get_config(config_path)
-    available_targets = get_buildable_targets(config)
-    if not target in available_targets:
-        print ("Error: target [{}] is not available. Please specify a valid target.".format(target), file=sys.stderr)
-        print ("  Available targets:\n\t{}".format("\n\t".join(sorted(available_targets))), file=sys.stderr)
-        sys.exit(ERROR_INVALID_TARGET)
+    check_target(config, target)
 
     variables = get_variables(config, target)
     build_template = Template(open(variables.get('template'), 'r').read())
     print (build_template.render({**variables, **PACKER_VARIABLES}))
+    sys.exit(ERROR_SUCCESS)
+
+@entry_point.command()
+@click.argument('target', required=True, nargs=1)
+@click.option('-c', '--config', 'config_path', default=DEFAULT_CONFIG_FILE, help="configuration file path")
+def variables(target, config_path):
+    """
+    """
+    # Check if specified target is in configuration
+    config = get_config(config_path)
+    check_target(config, target)
+
+    variables = get_variables(config, target)
+    print (json.dumps(variables))
     sys.exit(ERROR_SUCCESS)
 
 if __name__ == '__main__':
